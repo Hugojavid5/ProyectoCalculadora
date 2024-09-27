8eb32d83d00cadd030a12430f9947ac4358bdb76
Hugo <hugojavidferrero@gmail.com>
2024-09-27 10:44:17 +0200

    Html javadoc

---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.proyectocalculadora","l":"HelloApplication"},{"p":"org.example.proyectocalculadora","l":"HelloController"},{"p":"org.example.proyectocalculadora","l":"Operaciones"}];updateSearchResults();
